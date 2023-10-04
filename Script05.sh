1fdfd29a41e3b219da9060d9826cfa984f8f5784
UninvitedActivity <145982213+UninvitedActivity@users.noreply.github.com>
2023-10-04 21:22:37 +1030

    Delete Script05.sh

--- Script05.sh
+++
@@ -1,6 +0,0 @@
-awk -F"[,]" '{print "INSERT INTO `uninvitedActivity` (`activityDate`, `ipAddress`, `server`) VALUES ('\''"$1"'\'', '\''"$2"'\'', '\''"$3"'\'');"}' 20230928_DBImportReady.csv > ImportSQLCommands.sql
-
-while IFS= read -r line;
-do
-   mariadb -u <_user_> --password=<_password_> -h <_Host IP Address_> <_Database Name_> -e "$line";
-done < ImportSQLCommands.sql
