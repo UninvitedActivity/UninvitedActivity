9e11e6a7a2c5d147307c74ad2e3bfc6312108f59
Uninvited Activity <github@atcf.com.au>
2024-07-06 11:16:24 +0930

    Blocklist update: 11:14 on Saturday 06 July 2024

---
+++ 02_InternetScanners/singleListGenerator.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+rm 02_InternetScannerSingleList.txt
+
+echo "# AlphaStrikeLabs\n" >> 02_InternetScannerSingleList.txt
+cat 02_AlphaStrikeLabs.txt >> 02_InternetScannerSingleList.txt
+echo "#\n" >> 02_InternetScannerSingleList.txt
+
+echo "# AnubisNetworks\n" >> 02_InternetScannerSingleList.txt
+cat 02_AnubisNetworks >> 02_InternetScannerSingleList.txt
+echo "#\n" >> 02_InternetScannerSingleList.txt
+
+echo "# AlphaStrikeLabs\n" >> 02_InternetScannerSingleList.txt
+cat 02_CensysIO.txt >> 02_InternetScannerSingleList.txt
+echo "#\n" >> 02_InternetScannerSingleList.txt
+
+echo "# CriminalIP\n" >> 02_InternetScannerSingleList.txt
+cat 02_CriminalIP.txt >> 02_InternetScannerSingleList.txt
+echo "#\n" >> 02_InternetScannerSingleList.txt
+
+echo "# GatechEdu\n" >> 02_InternetScannerSingleList.txt
+cat 02_GatechEdu.txt >> 02_InternetScannerSingleList.txt
+echo "#\n" >> 02_InternetScannerSingleList.txt
+
+echo "# InternetAlbedo\n" >> 02_InternetScannerSingleList.txt
+cat 02_InternetAlbedo.txt >> 02_InternetScannerSingleList.txt
+echo "#\n" >> 02_InternetScannerSingleList.txt
+
+echo "# Internet-CensusOrg\n" >> 02_InternetScannerSingleList.txt
+cat 02_Internet-CensusOrg.txt >> 02_InternetScannerSingleList.txt
+echo "#\n" >> 02_InternetScannerSingleList.txt
+
+echo "# InternetMeasurement\n" >> 02_InternetScannerSingleList.txt
+cat 02_InternetMeasurement.txt >> 02_InternetScannerSingleList.txt
+echo "#\n" >> 02_InternetScannerSingleList.txt
+
+echo "# AlphaStrikeLabs\n" >> 02_InternetScannerSingleList.txt
+cat 02_InterneTTL.txt >> 02_InternetScannerSingleList.txt
+echo "#\n" >> 02_InternetScannerSingleList.txt
+
+echo "# IsiEduPinger\n" >> 02_InternetScannerSingleList.txt
+cat 02_IsiEduPinger.txt >> 02_InternetScannerSingleList.txt
+echo "#\n" >> 02_InternetScannerSingleList.txt
+
+echo "# LeakIXOrg\n" >> 02_InternetScannerSingleList.txt
+cat 02_LeakIXOrg.txt >> 02_InternetScannerSingleList.txt
+echo "#\n" >> 02_InternetScannerSingleList.txt
+
+echo "# PaloAltoNetworks\n" >> 02_InternetScannerSingleList.txt
+cat 02_PaloAltoNetworks.txt >> 02_InternetScannerSingleList.txt
+echo "#\n" >> 02_InternetScannerSingleList.txt
+
+echo "# Rapid7\n" >> 02_InternetScannerSingleList.txt
+cat 02_Rapid7.txt >> 02_InternetScannerSingleList.txt
+echo "#\n" >> 02_InternetScannerSingleList.txt
+
+echo "# Recyber\n" >> 02_InternetScannerSingleList.txt
+cat 02_Recyber.txt >> 02_InternetScannerSingleList.txt
+echo "#\n" >> 02_InternetScannerSingleList.txt
+
+echo "# SecurityTrails\n" >> 02_InternetScannerSingleList.txt
+cat 02_SecurityTrails.txt >> 02_InternetScannerSingleList.txt
+echo "#\n" >> 02_InternetScannerSingleList.txt
+
+echo "# ShadowServer\n" >> 02_InternetScannerSingleList.txt
+cat 02_ShadowServer.txt >> 02_InternetScannerSingleList.txt
+echo "#\n" >> 02_InternetScannerSingleList.txt
+
+echo "# Stretchoid\n" >> 02_InternetScannerSingleList.txt
+cat 02_Stretchoid.txt >> 02_InternetScannerSingleList.txt
+echo "#\n" >> 02_InternetScannerSingleList.txt
+
+# Use 'ls -1' and script this recursively to make it much more graceful
