e9159a55063dd8aaf282b01c3c971fe5defc7930
Uninvited Activity <github@atcf.com.au>
2024-07-16 22:01:22 +0930

    Blocklist update: 21:59 on Tuesday 16 July 2024

--- 02_InternetScanners/singleListGenerator.sh
+++ 02_InternetScanners/singleListGenerator.sh
@@ -1,73 +1,25 @@
 #!/bin/sh
 
-rm 02_InternetScannerSingleList.txt
+outputFile=02_ZZInternetScannersSingleList.txt
+inputFile=dirList.txt
 
-echo "# AlphaStrikeLabs\n" >> 02_InternetScannerSingleList.txt
-cat 02_AlphaStrikeLabs.txt >> 02_InternetScannerSingleList.txt
-echo "#\n" >> 02_InternetScannerSingleList.txt
+echo Output File: $outputFile
+echo Input File: $inputFile
+rm $outputFile
 
-echo "# AnubisNetworks\n" >> 02_InternetScannerSingleList.txt
-cat 02_AnubisNetworks >> 02_InternetScannerSingleList.txt
-echo "#\n" >> 02_InternetScannerSingleList.txt
+ls -1 02* | grep -v ZZ > $inputFile
 
-echo "# AlphaStrikeLabs\n" >> 02_InternetScannerSingleList.txt
-cat 02_CensysIO.txt >> 02_InternetScannerSingleList.txt
-echo "#\n" >> 02_InternetScannerSingleList.txt
+echo While loop beginning:
+while IFS= read -r line
+do
+   echo # >> $outputFile
+   cat $line >> $outputFile
+   echo # >> $outputFile
+   echo "" >> $outputFile
+done < "$inputFile"
+echo While loop ended.
 
-echo "# CriminalIP\n" >> 02_InternetScannerSingleList.txt
-cat 02_CriminalIP.txt >> 02_InternetScannerSingleList.txt
-echo "#\n" >> 02_InternetScannerSingleList.txt
+echo Removing temporary input file
+rm $inputFile
 
-echo "# GatechEdu\n" >> 02_InternetScannerSingleList.txt
-cat 02_GatechEdu.txt >> 02_InternetScannerSingleList.txt
-echo "#\n" >> 02_InternetScannerSingleList.txt
-
-echo "# InternetAlbedo\n" >> 02_InternetScannerSingleList.txt
-cat 02_InternetAlbedo.txt >> 02_InternetScannerSingleList.txt
-echo "#\n" >> 02_InternetScannerSingleList.txt
-
-echo "# Internet-CensusOrg\n" >> 02_InternetScannerSingleList.txt
-cat 02_Internet-CensusOrg.txt >> 02_InternetScannerSingleList.txt
-echo "#\n" >> 02_InternetScannerSingleList.txt
-
-echo "# InternetMeasurement\n" >> 02_InternetScannerSingleList.txt
-cat 02_InternetMeasurement.txt >> 02_InternetScannerSingleList.txt
-echo "#\n" >> 02_InternetScannerSingleList.txt
-
-echo "# AlphaStrikeLabs\n" >> 02_InternetScannerSingleList.txt
-cat 02_InterneTTL.txt >> 02_InternetScannerSingleList.txt
-echo "#\n" >> 02_InternetScannerSingleList.txt
-
-echo "# IsiEduPinger\n" >> 02_InternetScannerSingleList.txt
-cat 02_IsiEduPinger.txt >> 02_InternetScannerSingleList.txt
-echo "#\n" >> 02_InternetScannerSingleList.txt
-
-echo "# LeakIXOrg\n" >> 02_InternetScannerSingleList.txt
-cat 02_LeakIXOrg.txt >> 02_InternetScannerSingleList.txt
-echo "#\n" >> 02_InternetScannerSingleList.txt
-
-echo "# PaloAltoNetworks\n" >> 02_InternetScannerSingleList.txt
-cat 02_PaloAltoNetworks.txt >> 02_InternetScannerSingleList.txt
-echo "#\n" >> 02_InternetScannerSingleList.txt
-
-echo "# Rapid7\n" >> 02_InternetScannerSingleList.txt
-cat 02_Rapid7.txt >> 02_InternetScannerSingleList.txt
-echo "#\n" >> 02_InternetScannerSingleList.txt
-
-echo "# Recyber\n" >> 02_InternetScannerSingleList.txt
-cat 02_Recyber.txt >> 02_InternetScannerSingleList.txt
-echo "#\n" >> 02_InternetScannerSingleList.txt
-
-echo "# SecurityTrails\n" >> 02_InternetScannerSingleList.txt
-cat 02_SecurityTrails.txt >> 02_InternetScannerSingleList.txt
-echo "#\n" >> 02_InternetScannerSingleList.txt
-
-echo "# ShadowServer\n" >> 02_InternetScannerSingleList.txt
-cat 02_ShadowServer.txt >> 02_InternetScannerSingleList.txt
-echo "#\n" >> 02_InternetScannerSingleList.txt
-
-echo "# Stretchoid\n" >> 02_InternetScannerSingleList.txt
-cat 02_Stretchoid.txt >> 02_InternetScannerSingleList.txt
-echo "#\n" >> 02_InternetScannerSingleList.txt
-
-# Use 'ls -1' and script this recursively to make it much more graceful
+exit 0
